cae91417cd81222ae700e7e561431d8467a487ba
Danny <bublikplushka@yandex.ru>
2022-09-07 23:41:48 +0300

    Fix it for Savva

--- point.cpp
+++ point.cpp
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unordered_map>
-
+#include <map>
+#include <iostream>
 
 
 using point_id = std::pair<int, int>;
-using map_t = std::unordered_map<std::pair<int, int>, int>;
+using map_t = std::map<std::pair<int, int>, int>;
+
+int find_point(map_t point_map, int x, int y);
+void add_point(map_t &point_map, int x, int y, int col);
 
 int main()
 {
@@ -14,42 +17,69 @@ int main()
     scanf("%d", &number_of_requests);
     
     
-    std::unordered_map<std::pair<int, int>, int> point_map{{{-1, -1}, -1}};
+    map_t point_map{};
     int counter = 0; 
 
     int x, y, col;
     for (int i = 0; i < number_of_requests; i++)
     {
-        scanf("%d", &kind_of_request);
-        switch (kind_of_request)
-        {
-            case 1:
-                scanf("%d %d %d", &x, &y, &col);
-                add_point(point_map, x, y, col);
-                break;
-
-            case 2:
-                scanf("%d %d", &x, &y);
-                int color = find_point(point_map, x, y);
-                printf("%c", color);
-                break;
-
-            default:
-                printf("invalid input");
-                break;
+        // scanf("%d", &kind_of_request);
+        std::cin >> kind_of_request;
+        
+        if (kind_of_request == 1) {
+            //  scanf("%d %d %d", &x, &y, &col);
+            std::cin >> x >> y >> col;
+            add_point(point_map, x, y, col);
         }
+        else if (kind_of_request == 2) {
+            // scanf("%d %d ", &x, &y);
+            std::cin >> x >> y;
+            col = find_point(point_map, x, y);
+            std::cout << col << std::endl;
+            // printf("%c", col);
+        }
+        else {
+            std::cout << "INVALID YOU`RE DUMMY BOOOOY" << std::endl;
+        }
+        // switch (kind_of_request)
+        // {
+        //     case 1:
+        //         scanf("%d %d %d", &x, &y, &col);
+        //         add_point(point_map, x, y, col);
+        //         break;
+
+        //     case 2:
+        //         scanf("%d %d ", &x, &y);
+        //         col = find_point(point_map, x, y);
+        //         std::cout << "AAAAAAAAAAAAAAAA" << std::endl;
+        //         printf("%c", col);
+        //         break;
+
+        //     default:
+        //         printf("invalid input");
+        //         break;
+        // }
     }
     return 0;
 }
 //----------------------------------------------------------
-void add_point(map_t point_map, int x, int y, int col)
+void add_point(map_t &point_map, int x, int y, int col)
 {
     point_id point{x, y};
     point_map.insert({point, col});
 }
 //--------------------------------------------------------------
 int find_point(map_t point_map, int x, int y)
-{
+{   
     point_id point{x, y};
-    return point_map.find(point)->second;
+    auto it = point_map.find(point);
+
+    if (it != point_map.end()) {
+        // std::cout << it->first << *it->second << std::endl;
+        return it->second;
+    }
+    
+    std::cout << "not found" << std::endl;
+    return 0; 
+
 }
